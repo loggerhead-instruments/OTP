61158ca25a2301f5834c13387d32a56f23575d2c
David Mann <dmann@loggerhead.com>
2019-05-14 13:26:20 -0400

    OTP3 PCB

--- Arduino/OTP2/OTP2.ino
+++ Arduino/OTP2/OTP2.ino
@@ -6,7 +6,7 @@
 #include <prescaler.h>
 
 boolean tagID[32] = {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1};
-int16_t threshold = 100; // threshold for detecting signal
+int16_t threshold = 0; // threshold for detecting signal
 uint16_t thresholdCount = 100; // n values need to exceed threshold in one buffer to trigger
 
 
